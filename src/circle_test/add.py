f0c35029fa525513b28c1e8a4e6c9b26821625e3
Xiaohua Zhang <cheonghiuwaa@gmail.com>
2025-07-17 16:41:10 -0700

    support sub (#4)
    Co-authored-by: Xiaohua Zhang <xiaohua.dev@gmail.com>

--- src/circle_test/add.py
+++ src/circle_test/add.py
@@ -1,2 +1,6 @@
 def add(a: int, b: int) -> int:
     return a + b + 2
+
+
+def sub(a: int, b: int) -> int:
+    return a - b
--- tests/test_add.py
+++ tests/test_add.py
@@ -3,3 +3,7 @@ from circle_test import add
 
 def test_add() -> None:
     assert add(1, 2) == 3
+
+
+def test_sub() -> None:
+    assert sub(1, 2) == -1
